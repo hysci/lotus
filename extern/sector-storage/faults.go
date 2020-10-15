fe22bba6ed8b1e9f3564f151290969a45be7f60f
filecash <filecoincash@gmail.com>
2020-10-16 00:41:31 +0800

    filecash/v0.7.0

--- extern/sector-storage/faults.go
+++ extern/sector-storage/faults.go
@@ -98,15 +98,12 @@ func addCachePathsForSectorSize(chk map[string]int64, cacheDir string, ssize abi
 	case 8 << 20:
 		fallthrough
 	case 512 << 20:
-		chk[filepath.Join(cacheDir, "sc-02-data-tree-r-last.dat")] = 0
 	case 4 << 30:
-        for i := 0; i < 1; i++ {
-            chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
-        }
+		chk[filepath.Join(cacheDir, "sc-02-data-tree-r-last.dat")] = 0
 	case 16 << 30:
-        for i := 0; i < 4; i++ {
-            chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
-        }
+		for i := 0; i < 4; i++ {
+			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
+		}
 	case 32 << 30:
 		for i := 0; i < 8; i++ {
 			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
